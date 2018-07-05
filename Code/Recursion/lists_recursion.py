35e783ab333bd9738a6ef66915192ce41e05e898
Eric Wang <ericwang2004@gmail.com>
2018-07-05 15:25:08 -0400

    updated list recursion file

--- Code/Recursion/lists_recursion.py
+++ Code/Recursion/lists_recursion.py
@@ -5,3 +5,60 @@
 # searching for a number in a list
 # sum of a list
 
+def print_list_iterative(l):
+    while len(l) >= 1:
+        print(l[0])
+        l = l[1:]
+
+def print_list_recursive(l):
+    if len(l) >= 1:
+        print(l[0])
+    print_list_recursive(l[1:])
+
+def search_list_iterative(l, item):
+    i = 0
+    while i < len(l):
+        if l[i] == item:
+            return i
+        i += 1
+    return 'not found'
+
+def search_list_recursive(l, item):
+    if len(l) == 0:
+        # return ?
+        return 'not found'
+    elif l[0] == item: # if item is the first element, then return the index
+        # return 0
+        return 'found'
+    else:
+        return search_list_recursive(l[1:], item) # +1
+    
+def sum_list_iterative(l):
+    i = 0
+    total = 0
+    while i < len(l):
+        total += l[i]
+        i += 1
+    return total
+
+def sum_list_recursive(l):
+    if len(l) == 0:
+        return 0
+    else:
+        return sum_list_recursive(l[1:]) + l[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
