039d94804c89780815e7de51c956bfd7d1df61cb
Eric Wang <ericwang2004@gmail.com>
2018-07-06 14:21:24 -0400

    updated exponentiation

--- Code/Recursion/exponentiation.py
+++ Code/Recursion/exponentiation.py
@@ -11,21 +11,29 @@ def exp(a, b): # compute a**b
         a *= c
     return a
 
-def exp_recursive(a, b):
-    pass # friday
-
 def exp2(a, b): # iteratively
-    pass
+    result = 1
+    binary_b = bin(b)[2:]
+    for d in binary_b:
+        if d == 1:
+            result = result**2 * a
+        else: # d = 0
+            result = result**2
+    return result
 
-def exp2_recursive(a, b):
-    pass
+def exp2_recursive(a, binary_b):
+    #binary_b = bin(b)[2:]
+    # assume binary_b is already in binary
+    # this doesn't work
+    if len(binary_b) == 0:
+        return 1
+    else:
+        if binary_b[0] == '1':
+            return exp2_recursive(a, binary_b[1:])**2*a
+        else:
+            return exp2_recursive(a, binary_b[1:])**2
 
 
 
 
 
-
-start = time.perf_counter()
-print(exp(2, 10000))
-end = time.perf_counter()
-print(end-start)
