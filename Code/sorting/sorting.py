eb7d823ee08cadaf7730c05831bce4c474c001a5
Eric Wang <ericwang2004@gmail.com>
2018-07-15 19:59:25 -0400

    updated sorting

--- Code/sorting/sorting.py
+++ Code/sorting/sorting.py
@@ -4,6 +4,9 @@
 
 import time
 import random
+import sys
+
+sys.setrecursionlimit(100000000)
 
 def selection_sort(l):
     
@@ -34,6 +37,18 @@ def insertion_sort(l):
             i += 1 
         return insertion_sort(l)
 
+def insertion_iterative(l):
+    for i in range(1, len(l)): # for each element in l
+        j = i
+        # print(l)
+        while j > 0:
+            if l[j] < l[j-1]: # continue switching the selected element until it is correctly placed
+                l[j], l[j-1] = l[j-1], l[j]
+                j -= 1
+            else:
+                break
+    return l
+
 def merge(l1, l2):
     
     result = []
@@ -41,47 +56,28 @@ def merge(l1, l2):
     j = 0
 
     while i < len(l1) and j < len(l2):
-        if l1[i] <= l2[j]:
-            result.append(l1[i])
+        if l1[i] <= l2[j]:          # compare the elements of each list
+            result.append(l1[i])    # if l1[i] is larger, increment i
             i += 1
         else:
-            result.append(l2[j])
+            result.append(l2[j])    # if l2[j] is larger, increment j
             j += 1
 
-    for x in range(i, len(l1)):
-        result.append(l1[x])
+    for x in range(i, len(l1)):     # one list will have no elements left to be sorted
+        result.append(l1[x])        # and dump the elements of the other list into result
     for y in range(j, len(l2)):
         result.append(l2[y])
 
     return result
 
 def mergesort(l):
-    right = l[len(l)//2:]
+    right = l[len(l)//2:] # partition l into two roughly equally sized list
     left = l[:len(l)//2]
     if len(l) == 1:
         return l
     else:
-        return merge(mergesort(right), mergesort(left))
+        return merge(mergesort(right), mergesort(left)) # sort each half and merge them
 
-print(mergesort([1, 5, 3, 6]))
-
-#test_list = [random.randrange(1, 101) for x in range(1, 101)]
-#print(insertion_sort(test_list))
-
-'''
-start = time.perf_counter()
-
-end = time.perf_counter()
-
-
-start = time.perf_counter()
-
-end = time.perf_counter()
-
-
-start = time.perf_counter()
-
-end = time.perf_counter()
-'''
-
+l = [6, 5, 3, 2, 3]
+print(insertion_iterative(l))
 
