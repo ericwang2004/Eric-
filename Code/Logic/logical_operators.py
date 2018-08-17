0d1b4e173e2b2d898b798399abc027deebf85069
Eric Wang <ericwang2004@gmail.com>
2018-08-17 15:41:02 -0400

    update logical_operators

--- Code/Logic/logical_operators.py
+++ Code/Logic/logical_operators.py
@@ -8,9 +8,6 @@ def and_(p, q):
 
 def multiply(n):
 	# multiply n by 5/8
-	m = n & 7 # 7 = 0b 111, keep the shifted out bits
-	n = n>>3 # divide by 8
-	n = (n << 2) + n # multiply by 4 then add another n
-	m = m>>3
-	m = (m<<2)+m
-	print(n, m)
+	n = (n<<2) + n # multiply by 4 then add another n
+	n >>= 3 # divide by 8
+	return n
