52ca48c064ebdba1c295355f493a529b33f7ca51
Eric Wang <ericwang2004@gmail.com>
2018-09-07 19:00:59 -0400

    update huffman

--- Code/Heaps/huffman.py
+++ Code/Heaps/huffman.py
@@ -20,32 +20,8 @@ class HuffmanNode:
 	def is_leaf(self):
 		return self.char != 'special character'
 	
-	def contains(self, character):
-		# check if subtree contains character
-		# we need this for trace_path_to method
-		if self.char == character:
-			return True
-		elif self.is_leaf():
-			return False
-		if self.left != 0 and self.right == 0:
-			return self.left.contains(character)
-		if self.left == 0 and self.right != 0:
-			return self.right.contains(character)
-		if self.left != 0 and self.right != 0:
-			return self.left.contains(character) or self.right.contains(character)
-
-	def trace_path_to(self, character):
-		# determines the encoding of a character
-		code = '' # binary sequence
-		pointer = self
-		while pointer.char != character:
-			if pointer.left.contains(character):
-				code += '0'
-				pointer = pointer.left
-			else:
-				code += '1'
-				pointer = pointer.right
-		return code
+	def __repr__(self):
+		return '{'+self.char+' '+str(self.freq)+' '+repr(self.left)+repr(self.right)+'}'
 
 class HuffmanTree:
 
@@ -62,12 +38,6 @@ class HuffmanTree:
 	def __eq__(self, other):
 		return self.value == other.value
 	
-	def contains(self, character):
-		return self.root.contains(character)
-	
-	def trace_path_to(self, character):
-		return self.root.trace_path_to(character)			
-
 def combine(t1, t2):
 	# creates a new tree with root r with left and right descendants t1 and t2, resp. 
 	r = HuffmanNode('special character', t1.root.freq + t2.root.freq)
@@ -80,7 +50,7 @@ def get_frequencies(string):
 	# given a string, return a dictionary of the frequencies of every characterr
 	letters = {}
 	for c in string:
-		if c in letters.keys():
+		if c in letters:
 			letters[c] += 1
 		else:
 			letters[c] = 1
@@ -100,7 +70,7 @@ def construct_tree(freqs):
 		h.delete()
 		t2 = h.minimum()
 		h.delete()
-		newTree = combine(t1, t2) # create a new tree combining the two nodes
+		newTree = combine(t2, t1) # create a new tree combining the two nodes
 		h.insert(newTree) # insert the new tree back into the heap
 	# at this point, the heap contains only one element
 	return h.minimum()	
@@ -114,11 +84,18 @@ def encode_helper(current_node, current_bin, char_dict):
 	if current_node.right != 0: # same for right
 		encode_helper(current_node.right, current_bin+'1', char_dict)
 	
-def encode(string):
+def encode_dict(string):
 	tree = construct_tree(get_frequencies(string))
 	codes = {} # dictionary of encoded characters
 	encode_helper(tree.root, '', codes)
 	return codes
+
+def encode(string):
+	s = encode_dict(string)
+	encoded = ''
+	for char in string:
+		encoded += s[char]
+	return encoded
 
 def decode(bin_seq, tree):
 	# decode a given binary sequence
@@ -134,7 +111,8 @@ def decode(bin_seq, tree):
 			pointer = tree.root # and reset the position of pointer
 	return text
 
-
+string = 'g'*14+'e'*13+'l'*9+'i'*8+'w'*6+'ns'*3+'adry'*2+'qukp'
+t = construct_tree(get_frequencies(string))
 
 
 
