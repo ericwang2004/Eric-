1715db2b3282d4ee287a66407f8edb3350f09790
Eric Wang <ericwang2004@gmail.com>
2018-09-28 19:05:54 -0400

    update graphs

--- Code/Graphs/graphs.py
+++ Code/Graphs/graphs.py
@@ -1,41 +1,47 @@
-#
-#
-#
-#
-#
+# 
+# functions to create adj_list and adj_matrix
+# 
+# 
+# 
 
 
+
+def is_vertex(string):
+	return ' ' not in string
 
 
 
 f = open('graph.txt', 'r')
+lines = f.readlines()
 
 adj_list = {}
-element_list = []
-adj_matrix = []
-for line in f:
-	# if it's a vertex
-	if len(line) == 2:
-		adj_list[line.strip('\n')] = []
-		
-		element_list.append(line[0]) # to keep track of row and column headings
-		adj_matrix.append([]) # create new row at each new vertex
-		for row in adj_matrix:
-			while len(row) <= len(element_list)-1:
-				row.append(0)
-		# print(adj_matrix)
+element_list = {}
 
-	# if it's an edge
-	elif len(line) == 4:
+count = 0
+enc_edge = False
+
+for line in lines:
+	line = line.strip()
+	# if it's a vertex
+	if is_vertex(line):
+		adj_list[line] = []
 		
-		adj_list[line[0]].append(line[2])
-		adj_matrix[element_list.index(line[0])][element_list.index(line[2])] = 1
+		element_list[line] = count  # to keep track of row and column headings
+		count += 1
+	# if it's an edge
+	else:
+		if not enc_edge:
+			adj_matrix = [[] for i in range(count)] 
+		
+		v1, v2 = line.split(' ')		
+		adj_list[v1].append(v2)
+		adj_matrix[element_list[v1]][element_list[v2]] = 1
 		
 #print(adj_list)
 #print(adj_matrix)
 
 
-
+f.close()
 
 
 
