43ceb70c034d29e16d81c029d7e10e3259d85c06
Eric Wang <ericwang2004@gmail.com>
2018-10-12 19:21:32 -0400

    update word_paths

--- Code/Graphs/word_paths.py
+++ Code/Graphs/word_paths.py
@@ -1,10 +1,29 @@
-#
-#
-#
-#
+# comment this
+# find the longest shortest path between two words
+# runtime of BFS, DFS, create_adjacency_list
+# DFS
 #
 
+from collections import deque
 
+
+
+def BFS(graph, start, end):
+    # determines whether there exists a path from start to end
+    # graph is an adjacency list
+	l = deque() 
+	l.append([start])
+	visited = set([start])
+	while l: # > 0     
+		path = l.popleft()
+		if path[-1] == end:
+			return path
+		visited.add(path[-1])
+		
+		for neighbor in graph[path[-1]]:
+			if neighbor not in visited:
+				l.append(path + [neighbor])
+	return -1
 
 def connected(w1, w2):
 	# check if w1 and w2 differ by exactly 1 letter
@@ -18,22 +37,31 @@ def connected(w1, w2):
 	return True
 
 def create_adjacency_list(lines):
+
 	adj = {}
+	d = {}
 	for word in lines:
+		word = word.strip()
 		adj[word] = []
-	for word in lines:
-		for key in adj:
-			if connected(word, key):
-				adj[key].append(word)
+		for i in range(len(word)):
+			word2 = word[:i] + '?' + word[i+1:]
+			if word2 not in d:
+				d[word2] = [word]
+			else:
+				d[word2].append(word)
+	
+	for i in d:
+		related = d[i]
+		for a in range(len(related)):
+			for b in range(len(related)):
+				if a != b:
+					adj[related[a]].append(related[b])
+
 	return adj
 
-def exists_word_path(w1, w2):
-	# 6 letter words
-	f = open('6-letter-words.txt', 'r')
-	lines = f.readlines()
-	adjl = create_adjacency_list(lines)
-	
-	return exists_path_BFS(adjl, w1, w2)
+f = open('6-letter-words.txt', 'r')
+lines = f.readlines()
+adjl = create_adjacency_list(lines)
 
 
 
