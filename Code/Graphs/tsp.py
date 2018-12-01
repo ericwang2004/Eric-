385aee33ddf0c78ec66a5fa085730fced8537812
Eric Wang <ericwang2004@gmail.com>
2018-11-30 19:10:55 -0500

    update tsp

--- Code/Graphs/tsp.py
+++ Code/Graphs/tsp.py
@@ -31,30 +31,32 @@ from itertools import permutations
 def d(G, v1, v2):
 	# returns the distance between v1 and v2 in a graph
 	# G is adjacency list = {v:[(v1, d1)]}
-	for i in range(len(G[v1])):
-		if G[v1][i][0] == v2:
-			return G[v1][i][1]
+	# O(V), since len(G[v1]) <= V
+	for vertex, distance in G[v1]:
+		if vertex == v2:
+			return distance
 
 def tsp(G):
 	# G is adjacency list
 	size = len(G)
+	L =  list(G.keys())
+	start = L[0]
+	del L[0]
 	min_w = 100000 # this will store the current minimum weighted path
 	best_path = () # this will store the best path
-	for path in permutations(G):
+	for path in permutations(L):
+
 		# iterate through all possible orders in which vertices can be visited
-		w = 0 # weight of the path
-		for i in range(size):
-			if i == size-1:
-				# the last one
-				# complete the cycle by connecting the first and last vertices visited
-				w += d(G, path[0], path[-1])
-			else:
-				w += d(G, path[i], path[i+1])
+		w = d(G, start, path[0]) # weight of the path
+		for i in range(size-2):
+			w += d(G, path[i], path[i+1])
+		w += d(G, path[-1], start)
 		if w < min_w:
 			# if this new path is better than the previous, replace
 			min_w = w
 			best_path = path
-
+		
+	best_path = (start,) + best_path
 	return best_path, min_w
 
 # print(tsp(G))
